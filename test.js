8c7cdfa9f6f4c66defd23c246f54e7af7bde91ef
phuonghv <phuong@temona.co.jp>
2017-11-18 12:24:03 +0900

    sadd

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   $("body").fireworks();
+  for (i=0; i < 6; i++){
+    var obj = document.createElement("img")
+    obj.src="b1.png"
+    $("#ballon").append(obj)
+  }
 })
